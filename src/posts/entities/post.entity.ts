ac9cb4d0d6d69aab5154988cc4f1bde3349908d9
Dmitry Kanaev <dimmkan@mail.ru>
2023-05-15 12:11:16 +0300

    Entity

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,25 @@
-export class Post {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'posts' })
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  header: string;
+
+  @Column()
+  body: string;
+
+  @CreateDateColumn()
+  date_create: Date;
+
+  @UpdateDateColumn()
+  date_update: Date;
+}

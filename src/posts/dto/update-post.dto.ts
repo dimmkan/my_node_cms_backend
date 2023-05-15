bbebaa3cfdde6dcca4a1ee2496912f63e08b1e4b
Dmitry Kanaev <dimmkan@mail.ru>
2023-05-15 12:30:32 +0300

    Fix DTO

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,12 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
-import { IsNumber, IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class UpdatePostDto extends PartialType(CreatePostDto) {
-  @IsNumber()
-  @IsNotEmpty()
-  id: number;
-
   @IsString()
   @IsNotEmpty()
   header: string;
